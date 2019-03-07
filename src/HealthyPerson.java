3f24250d63cccb51adc007d691350f7b0ed8adb5
Hana <hanabananak@gmail.com>
2019-03-07 14:29:14 -0600

    Created HealthyPerson class

---
+++ src/HealthyPerson.java
@@ -0,0 +1,26 @@
+
+public class HealthyPerson extends Person
+{
+
+    private String reason;
+    
+    public HealthyPerson(String name, int age, String reason)
+    {
+        super(name, age);
+        this.reason = reason;
+    }
+
+    @Override
+    protected int compareToImpl(Person p)
+    {
+        compareTo(p);
+        return 0;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return String.format("%s In for %s", toString(), reason);
+    }
+
+}
